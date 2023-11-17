506fd310b0db67b9a3373ff4aa347b674b101de7
MariaBertoni <129201440+MariaBertoni@users.noreply.github.com>
2023-11-17 15:34:04 -0300

    img

--- quitanda.py
+++ quitanda.py
@@ -50,6 +50,10 @@ def login():
     title="Login"
     return render_template("login.html", title=title)
 
+@app.route("/sobre")
+def sobre():
+    return render_template("quemsomos.html")
+
 @app.route("/acesso", methods=['POST'])
 def acesso():
     global usuario, senha
@@ -95,20 +99,72 @@ def cadprodutos():
 @app.route("/cadastro", methods=["post"])
 def cadastro():
     if verifica_sessao:
+        id_prod=request.form['id_prod']
         nome_prod=request.form['nome_prod']
         desc_prod=request.form['desc_prod']
         preco_prod=request.form['preco_prod']
         img_prod=request.files['img_prod']
         id_foto=str(uuid.uuid4().hex)
         filename=id_foto+nome_prod+'.png'
-        img_prod.save("static/img/produtos"+filename)
+        img_prod.save("static/img/produtos/"+filename)
         conexao = conecta_database()
-        conexao.execute('INSERT INTO produtos (nome_prod, desc_prod, preco_prod, img_prod) VALUES (?, ?, ?, ?)', (nome_prod, desc_prod, preco_prod, filename))
+        conexao.execute('INSERT INTO produtos (nome_prod, desc_prod, preco_prod, img_prod, id_prod) VALUES (?, ?, ?, ?)', (nome_prod, desc_prod, preco_prod, filename, id_prod))
         conexao.commit() #Confirma a alteração no BD
         conexao.close()
         return redirect('/adm')
     else:
         return redirect('/login') # Vai para HOME
+    
+#ROTA DE EXCLUSÃO
+@app.route("/excluir/<id>")
+def excluir(id):
+    if verifica_sessao:
+        id = int(id)
+        conexao = conecta_database()
+        conexao.execute('DELETE FROM produtos WHERE id_prod = ?',(id,))
+        conexao.commit()
+        conexao.close()
+        return redirect('/adm')
+    else:
+        return redirect('/login')
+
+@app.route("/editprodutos/<id>")
+def editar(id_prod): 
+    if verifica_sessao():
+        iniciar_db()
+        conexao = conecta_database()
+        produtos = conexao.execute('SELECT * FROM produtos WHERE id_prod = ?',(id_prod,)).fetchall()
+        conexao.close()
+        title = "Edição de produtos"
+        return render_template("editprodutos.html",produtos=produtos,title=title)
+    else:
+        return redirect("/login")
+    
+@app.route("/editarprodutos", methods=['POST'])
+def editprod():
+    id_prod =  request.form['id_prod']
+    nome_prod=request.form['nome_prod']
+    desc_prod=request.form['desc_prod']
+    preco_prod=request.form['preco_prod']
+    img_prod=request.files['img_prod']
+    conexao = conecta_database()
+    if img_prod:
+        produto = conexao.execute('SELECT * FROM produtos')
+        filename= produto[0]['img_prod']
+        img_prod.save("static/img/produtos/"+filename)
+        conexao.execute('UPDATES produtos SET nome_prod = ?, desc_prod = ?, preco_prod = ?, img_prod = ? WHERE id_prod = ?',(nome_prod, desc_prod, preco_prod, filename, id_prod))
+        conexao.commit() #Confirma a alteração no BD
+        conexao.close()
+        return redirect('/adm')
+
+@app.route("/busca",methods=["post"])
+def busca():
+    busca=request.form['buscar']
+    conexao = conecta_database()
+    produtos = conexao.execute('SELECT * FROM produtos WHERE nome_prod LIKE "%" || ? || "%"',(busca,)).fetchall()
+    title = "Home"
+    render_template("home.html", produtos=produtos, title=title)
+
 
 # ------------------------------ FINAL DO CODIGO - EXECUTANDO O SERVIDOR ------------------------------
 
